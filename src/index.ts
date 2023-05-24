ebe952d02fdffec7abcdcd6ac38548782e7c68f0
dev-pengi <siffffdoul@gmail.com>
2023-05-24 06:03:41 +0100

    Add customization to the module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
+export interface MarkdownParserOptions {
+    parseList?: boolean;
+    parseCodeBlock?: boolean;
+    parseBlockQuote?: boolean;
+    parseHeading?: boolean;
+    parseInlineCode?: boolean;
+    parseHorizontalRule?: boolean;
+    parseBold?: boolean;
+    parseItalic?: boolean;
+    parseUnderline?: boolean;
+    parseStrikethrough?: boolean;
+    parseLink?: boolean;
+    parseImage?: boolean;
+}
+
 export class MarkdownParser {
+    private options: MarkdownParserOptions;
+
+    constructor(options: MarkdownParserOptions = {}) {
+        this.options = {
+            parseList: options.parseList !== undefined ? options.parseList : true,
+            parseCodeBlock: options.parseCodeBlock !== undefined ? options.parseCodeBlock : true,
+            parseBlockQuote: options.parseBlockQuote !== undefined ? options.parseBlockQuote : true,
+            parseHeading: options.parseHeading !== undefined ? options.parseHeading : true,
+            parseInlineCode: options.parseInlineCode !== undefined ? options.parseInlineCode : true,
+            parseHorizontalRule: options.parseHorizontalRule !== undefined ? options.parseHorizontalRule : true,
+            parseBold: options.parseBold !== undefined ? options.parseBold : true,
+            parseItalic: options.parseItalic !== undefined ? options.parseItalic : true,
+            parseUnderline: options.parseUnderline !== undefined ? options.parseUnderline : true,
+            parseStrikethrough: options.parseStrikethrough !== undefined ? options.parseStrikethrough : true,
+            parseLink: options.parseLink !== undefined ? options.parseLink : true,
+            parseImage: options.parseImage !== undefined ? options.parseImage : true,
+        };
+    }
+
     public parse(markdown: string): string {
         const lines: string[] = markdown.trim().split('\n');
         let html: string = '';
@@ -10,8 +44,7 @@ export class MarkdownParser {
         for (let i = 0; i < lines.length; i++) {
             const line: string = lines[i];
 
-
-            if (line.match(/^#{1,6}\s.+/)) {
+            if (this.options.parseHeading && line.match(/^#{1,6}\s.+/)) {
                 const level = line.indexOf(' ');
 
                 if (level > 0) {
@@ -22,7 +55,7 @@ export class MarkdownParser {
                 }
             }
 
-            if (line.match(/^(\s*)([-*]+|\d+\.)\s(.+)/)) {
+            if (this.options.parseList && line.match(/^(\s*)([-*]+|\d+\.)\s(.+)/)) {
                 const [, indent, listMarker, listItemText] = line.match(/^(\s*)([-*]+|\d+\.)\s(.+)/) ?? [];
                 const indentLevel = indent?.length / 2 ?? 0;
 
@@ -45,7 +78,7 @@ export class MarkdownParser {
                     html += '</li></ul>'.repeat(listIndentLevel - indentLevel) + `<li>${this.formatInlineElements(listItemText)}`;
                     listIndentLevel = indentLevel;
                 }
-            } else if (line.startsWith('```')) {
+            } else if (this.options.parseCodeBlock && line.startsWith('```')) {
                 if (isInCodeBlock) {
                     html += '</code></pre>';
                     isInCodeBlock = false;
@@ -53,18 +86,18 @@ export class MarkdownParser {
                     html += '<pre><code>';
                     isInCodeBlock = true;
                 }
-            } else if (line.startsWith('`')) {
+            } else if (this.options.parseInlineCode && line.startsWith('`')) {
                 html += '<code>' + this.escapeHtml(line.slice(1));
-            } else if (line.startsWith('---') || line.startsWith('***') || line.startsWith('___')) {
+            } else if (this.options.parseHorizontalRule && (line.startsWith('---') || line.startsWith('***') || line.startsWith('___'))) {
                 html += '<hr>';
-            } else if (line.startsWith('>')) {
+            } else if (this.options.parseBlockQuote && line.startsWith('>')) {
                 if (!isInBlockQuote) {
                     html += '<blockquote>';
                     isInBlockQuote = true;
                 }
 
                 html += this.formatInlineElements(line.slice(1).trim()) + ' ';
-            } else if (line.match(/^!\[.*?\]\(.*?\)/)) {
+            } else if (this.options.parseImage && line.match(/^!\[.*?\]\(.*?\)/)) {
                 const [, altText, imageUrl] = line.match(/^!\[(.*?)\]\((.*?)\)/) ?? [];
                 html += `<img src="${imageUrl}" alt="${altText}">`;
             } else {
@@ -82,8 +115,6 @@ export class MarkdownParser {
                     html += this.formatInlineElements(line) + ' ';
                 }
             }
-
-
         }
 
         if (isInList) {
@@ -101,17 +132,24 @@ export class MarkdownParser {
         return html;
     }
 
-
-
-
     private formatInlineElements(text: string): string {
         let formattedText = text;
 
-        formattedText = formattedText.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
-        formattedText = formattedText.replace(/\*(.*?)\*/g, '<em>$1</em>');
-        formattedText = formattedText.replace(/__(.*?)__/g, '<u>$1</u>');
-        formattedText = formattedText.replace(/~~(.*?)~~/g, '<s>$1</s>');
-        formattedText = formattedText.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
+        if (this.options.parseBold) {
+            formattedText = formattedText.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+        }
+        if (this.options.parseItalic) {
+            formattedText = formattedText.replace(/\*(.*?)\*/g, '<em>$1</em>');
+        }
+        if (this.options.parseUnderline) {
+            formattedText = formattedText.replace(/__(.*?)__/g, '<u>$1</u>');
+        }
+        if (this.options.parseStrikethrough) {
+            formattedText = formattedText.replace(/~~(.*?)~~/g, '<s>$1</s>');
+        }
+        if (this.options.parseLink) {
+            formattedText = formattedText.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
+        }
 
         return formattedText;
     }
